b7995e2332bc962e0dbc8c814601a60c9c1573b9
kektola70-sketch <kektola70@gmail.com>
2026-04-30 16:18:28 +0300

    update main.js

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
 
 function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
     backgroundColor: '#000000',
-    resizable: false, // Запретим менять размер, чтобы сохранить стиль Undertale
+    fullscreenable: true, 
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false
@@ -13,13 +13,12 @@ function createWindow() {
   });
 
   win.loadFile('index.html');
-  
-  // Убираем стандартное меню
   win.setMenu(null);
+
+  // Обработка закрытия из игры
+  ipcMain.on('close-app', () => {
+    app.quit();
+  });
 }
 
 app.whenReady().then(createWindow);
-
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit();
-});
